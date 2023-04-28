a3e17acaee19143c0da475c0123b7de06a33dcb5
machadopatricia <patsouzam@gmail.com>
2023-04-28 02:51:33 -0300

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "patricia-cloud"
 
     workspaces {
       name = "learn-terraform-github-actions"
@@ -24,7 +24,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1"
 }
 
 resource "random_pet" "sg" {}
@@ -55,7 +55,7 @@ resource "aws_instance" "web" {
               apt-get update
               apt-get install -y apache2
               sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-              echo "Hello World" > /var/www/html/index.html
+              echo "Hello Worrrrrrld" > /var/www/html/index.html
               systemctl restart apache2
               EOF
 }
